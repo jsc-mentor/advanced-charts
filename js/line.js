92da2fcb2a2f2a15e3cfba4879d5508863c0f2d2
Paul Franz <paulkfranz@gmail.com>
2016-10-06 19:14:23 -0400

    initial commit

---
+++ js/line.js
@@ -0,0 +1,39 @@
+$(function () {
+    $('#hcContainer').highcharts({
+        title: {
+            text: 'Monthly Average Temperature',
+            x: -20 //center
+        },
+        subtitle: {
+            text: 'Source: WorldClimate.com',
+            x: -20
+        },
+        xAxis: {
+            categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+                'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+        },
+        yAxis: {
+            title: {
+                text: 'Temperature (°C)'
+            },
+            plotLines: [{
+                value: 0,
+                width: 1,
+                color: '#808080'
+            }]
+        },
+        tooltip: {
+            valueSuffix: '°C'
+        },
+        legend: {
+            layout: 'vertical',
+            align: 'right',
+            verticalAlign: 'middle',
+            borderWidth: 0
+        },
+        series: [{
+            name: 'Widgets',
+            data: [7.0, 6.9, 9.5, 14.5]
+        }]
+    });
+});
